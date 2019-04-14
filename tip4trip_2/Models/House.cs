0ee45de5b24fdcc5c1eccd3600c7cc694a68ae8e
muntan <muntan@MUNTAN-PC>
2019-04-14 23:30:51 +0300

    tetarto commit

--- tip4trip_2/Models/House.cs
+++ tip4trip_2/Models/House.cs
@@ -11,7 +11,8 @@ namespace tip4trip_2.Models
         public int Id { get; set; }
         [Required(ErrorMessage ="Give the Address please !")]
         public string Address { get; set; }
-        
+        //int y = Convert.ToInt32(DateTime.Now);
+        [RangeAttribute(typeof(DateTime),"1/1/19","3/5/19")]
         [DataType(DataType.Date)]
         [Display(Name ="bale")]
         [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}",ApplyFormatInEditMode =true)]
