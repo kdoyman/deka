eb2c825dfe8929af0786c681a451975f30fa8c9d
muntan <muntan@MUNTAN-PC>
2019-04-16 18:41:31 +0300

    search impruve commit

--- tip4trip_2/Controllers/HomeController.cs
+++ tip4trip_2/Controllers/HomeController.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tip4trip_2.Models;
 
 namespace tip4trip_2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+       private T4TContext db = new T4TContext();
+
+        public ActionResult Index(string searching)
         {
-            return View();
+            return View(db.Houses.Where(x=>x.Address.Contains(searching) || searching==null).ToList());
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.not";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page.nooot";
 
             return View();
         }
