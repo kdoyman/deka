6ee13e9672215660b14420dc660645d462972b8e
Dimitris Paganias <dimpaganias@hotmail.com>
2019-04-17 20:59:58 +0300

    Create a new model class : Availability

--- tip4trip_2/Models/House.cs
+++ tip4trip_2/Models/House.cs
@@ -15,7 +15,7 @@ namespace tip4trip_2.Models
         [RangeAttribute(typeof(DateTime),"1/1/19","3/5/19")]
         [DataType(DataType.Date)]
         [Display(Name ="bale")]
-        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}",ApplyFormatInEditMode =true)]
+        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}",ApplyFormatInEditMode =true)]//jjkkk
         public DateTime DateToRent { get; set; }
     }
 }
